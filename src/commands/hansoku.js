6e62278dc8da5ab26b91d3bb3df8df833f6917c7
kevin535nivek <yo@yo.com>
2020-03-21 18:18:42 -0700

    Fix hansoku classification and lower the level

--- src/commands/hansoku.js
+++ src/commands/hansoku.js
@@ -1,9 +1,9 @@
 module.exports = {
   meta: {
     help: 'Senpai is giving you a hansoku.',
-    usage: '',
-    module: 'Util',
-    level: 6
+    usage: '@user',
+    module: 'Fun',
+    level: 4
   },
   fn: (msg, suffix) => {
     if (suffix.includes('@')) return msg.channel.createMessage('Senpai is giving ' + suffix + ' a hansoku for their insolence')
